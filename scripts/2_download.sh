2c1d94c8ea76f3b117b4d3b7cd13fd273cb2313a
pockyee <baoqi85@gmail.com>
2025-07-06 22:13:42 +0000

    update mechanism to overwrite the dataset in bash

--- scripts/2_download.sh
+++ scripts/2_download.sh
@@ -13,6 +13,7 @@ UA="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, l
 
 mkdir -p "$PROJECT_ROOT/raw_data"
 cd "$PROJECT_ROOT/raw_data"
+rm -f x_train.csv y_train.csv
 wget --header="Cookie: $COOKIE" --header="User-Agent: $UA" "https://challengedata.ens.fr/participants/challenges/35/download/x-train" -O x_train.csv
 wget --header="Cookie: $COOKIE" --header="User-Agent: $UA" "https://challengedata.ens.fr/participants/challenges/35/download/y-train" -O y_train.csv
 # wget --header="Cookie: $COOKIE" --header="User-Agent: $UA" "https://challengedata.ens.fr/participants/challenges/35/download/x-test" -O x_test.csv
